db1f46b1719a39551722c0ce9785a0fc8fd3bace
Bryan Maxwell Dobberstein <max.dobberstein@gmail.com>
2017-06-05 17:13:47 -0500

    add db persitence

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-from flask import Flask, request
-import os, jinja2
-import cgi
+from flask import Flask, request, redirect, render_template
+
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://todo:getitdone@localhost:3306/todo'
+app.config['SQLALCHEMY_ECHO'] = True
 
-template_dir = os.path.join(os.path.dirname(__file__), "templates")
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+db = SQLAlchemy(app)
 
-todo_list = []
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(200))
+
+    def __init__(self, name):
+        self.name = name
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
@@ -16,9 +22,21 @@ def index():
     if request.method == "POST":
         task_to_add = request.form['task']
         if task_to_add != "":
-            todo_list.append(task_to_add)
-    template = jinja_env.get_template("todo.html")
-    return template.render(title = title, task = todo_list)
+            new_task = Task(task_to_add)
+            db.session.add(new_task)
+            db.session.commit()
+    tasks = Task.query.all()
+
+    return render_template("todo.html", title = title, task = tasks)
+
+@app.route("/delete_task", methods = ["POST"])
+def delete_task():
+    task_to_delete = int(request.form["task_id"])
+    task = Task.query.get(task_to_delete)
+    db.session.delete(task)
+    db.session.commit()
+
+    return redirect("/")
 
 def main():
     app.run()
