087db33e378ff72ac552746a70aeb8e2c192a298
Bryan Maxwell Dobberstein <max.dobberstein@gmail.com>
2017-06-05 19:47:27 -0500

    tasks can be added, marked as complete or incomplete

--- main.py
+++ main.py
@@ -12,9 +12,11 @@ db = SQLAlchemy(app)
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(200))
+    completed = db.Column(db.Boolean)
 
-    def __init__(self, name):
+    def __init__(self, name, completed = False):
         self.name = name
+        self.completed = completed
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
@@ -25,15 +27,27 @@ def index():
             new_task = Task(task_to_add)
             db.session.add(new_task)
             db.session.commit()
-    tasks = Task.query.all()
+    tasks = Task.query.filter_by(completed = False).all()
+    complete_tasks = Task.query.filter_by(completed = True).all()
 
-    return render_template("todo.html", title = title, task = tasks)
+    return render_template("todo.html", title = title, task = tasks, complete_tasks = complete_tasks)
 
 @app.route("/delete_task", methods = ["POST"])
 def delete_task():
     task_to_delete = int(request.form["task_id"])
     task = Task.query.get(task_to_delete)
-    db.session.delete(task)
+    task.completed = True
+    db.session.add(task)
+    db.session.commit()
+
+    return redirect("/")
+
+@app.route("/add_task", methods = ["POST"])
+def add_task():
+    task_to_add = int(request.form["task_add"])
+    task = Task.query.get(task_to_add)
+    task.completed = False
+    db.session.add(task)
     db.session.commit()
 
     return redirect("/")
